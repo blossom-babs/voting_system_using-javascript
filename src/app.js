46961cc5fec820715e8a7a8e78a7da13f72c8332
Blossom Babalola <sapashb@gmail.com>
2020-08-19 16:06:49 +0100

    javascript nearly done

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
-console.log('hello world')
-const goToLB = document.getElementById('go-to-lb')
-const votePage = document.getElementById('vote-pg')
-const leaderBdPage = document.getElementById('leaderbd')
-const goToVote = document.getElementById('go-to-vote')
+function eventListeners() {
+  const goToLB = document.getElementById('go-to-lb')
+  const votePage = document.getElementById('vote-pg')
+  const leaderBdPage = document.getElementById('leaderbd')
+  const goToVote = document.getElementById('go-to-vote')
+  const vs = new voteSys()
 
-goToLB.addEventListener('click', () =>{
-    console.log(`yo what is going on`)
+  goToLB.addEventListener('click', () => {
+
+    vs.getVoteCuml()
+
     leaderBdPage.classList.remove('d-none')
-votePage.classList.add('d-none')
+    votePage.classList.add('d-none')
 
-})
+  })
 
-goToVote.addEventListener('click', () => {
-leaderBdPage.classList.add('d-none')
-votePage.classList.remove('d-none')
+  goToVote.addEventListener('click', () => {
+    leaderBdPage.classList.add('d-none')
+    votePage.classList.remove('d-none')
 
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const vote_sys = new voteSys()
+  vote_sys.getInputs()
+  vote_sys.getDecr()
+  eventListeners();
 })
--- src/index.js
+++ src/index.js
@@ -1,51 +1,112 @@
 class voteSys {
-    constructor() {
-        this.availableVote = document.getElementById('available-vote')
-        this.progressBar = document.getElementById('progressbar')
-        this.inputs = document.querySelectorAll('input')
-        this.plusBtns = document.querySelectorAll('.fa-plus')
-        this.minusBtns = document.querySelectorAll('.fa-minus')
-        this.currVal = 0
-    }
+  constructor() {
+    this.availableVote = document.getElementById('available-vote')
+    this.dashboard = document.getElementById('go-to-lb')
+    this.progressBar = document.getElementById('progressbar')
+    this.inputs = document.querySelectorAll('input')
+    this.plusBtns = document.querySelectorAll('.fa-plus')
+    this.minusBtns = document.querySelectorAll('.fa-minus')
+    this.vt = document.getElementById('vt')
+    this.lbd = document.getElementById('lbd')
+    this.voteCuml = {}
+    this.currVal = 0
+    this.remainingVote = ''
+  }
 
-    totalVote() {
-        let totalV = this.availableVote.textContent
-        this.progressBar.style.width = `${totalV}%`
-        return totalV
-    }
 
-    getInputs() {
-        for (let i = 0; i < this.plusBtns.length; i++) {
-            this.plusBtns[i].addEventListener('click', () => {
-                if (this.availableVote.textContent > 0) {
-                    // this.inputs[i].value = Number(++this.currVal);
-                    this.inputs[i].value++
-                    let remainingVote = this.totalVote()
-                    this.availableVote.textContent = Number(remainingVote) - 1
-                }
-            })
+
+  getInputs() {
+    for (let i = 0; i < this.plusBtns.length; i++) {
+      this.plusBtns[i].addEventListener('click', () => {
+        if (this.availableVote.textContent > 0) {
+          this.inputs[i].value++
+          this.remainingVote = this.totalVote()
+          this.availableVote.textContent = Number(this.remainingVote) - 1
+          this.voteCuml[i] = this.inputs[i].value
+          //console.log(this.voteCuml)
+
+        } else {
+          this.plusBtns[i].disabled = true
+          this.vt.classList.remove('d-none')
+          const self = this
+          setTimeout(() => {
+            self.vt.classList.add('d-none')
+          }, 4000)
         }
+
+      })
     }
 
-    getDecr() {
-        for (let i = 0; i < this.plusBtns.length; i++) {
-            this.minusBtns[i].addEventListener('click', () => {
-                if (this.inputs[i].value-- <= 0) {
-                    // this.inputs[i].value = Number(++this.currVal);
-                    this.inputs[i].value--
-                    let remainingVote = this.totalVote()
-                    this.availableVote.textContent = Number(remainingVote) + 1
-                }
-            })
+  }
+
+  totalVote() {
+    let totalV = this.availableVote.textContent
+    this.progressBar.style.width = `${totalV}%`
+    return totalV
+  }
+
+  getDecr() {
+    for (let i = 0; i < this.plusBtns.length; i++) {
+      this.minusBtns[i].addEventListener('click', () => {
+        if (this.inputs[i].value > 0) {
+          this.inputs[i].value--
+          this.remainingVote = this.totalVote()
+          this.availableVote.textContent = Number(this.remainingVote) + 1
         }
+      })
     }
+  }
+
+  getVoteCuml() {
+    this.dashboard.addEventListener('click', () => {
+      let indexScore = this.voteCuml
+      console.log(Object.entries(indexScore).sort((a, b) => b[1] - a[1]))
+      let flatten = Object.values(this.voteCuml)
+      console.log(flatten)
+      if (flatten.length > 0) {
+        const toNumbers = flatten.map(Number).reduce((acc, currVal) => acc + currVal)
+      }
+
+      if (this.availableVote.textContent > 0) {
+        this.lbd.classList.remove('d-none')
+        const selfLbd = this
+        setTimeout(() => {
+          selfLbd.lbd.classList.add('d-none')
+        }, 4000)
+      }
+
+      console.log(this.availableVote.textContent)
+      if (this.availableVote.textContent == 0) eventListeners();
+    })
+
+
+  }
 }
 
+function eventListeners() {
+  const goToLB = document.getElementById('go-to-lb')
+  const votePage = document.getElementById('vote-pg')
+  const leaderBdPage = document.getElementById('leaderbd')
+  const goToVote = document.getElementById('go-to-vote')
 
 
+  goToLB.addEventListener('click', () => {
+    leaderBdPage.classList.remove('d-none')
+    votePage.classList.add('d-none')
+
+  })
+
+  goToVote.addEventListener('click', () => {
+    leaderBdPage.classList.add('d-none')
+    votePage.classList.remove('d-none')
+
+  })
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    const vote_sys = new voteSys()
-    vote_sys.getInputs()
-    vote_sys.getDecr()
+  const vote_sys = new voteSys()
+  vote_sys.getInputs()
+  vote_sys.getDecr()
+  vote_sys.getVoteCuml()
+  //eventListeners();
 })
